1f07cc45d52768e9f208f0c3c7d42bda146a6865
Neeraj-Botla <neerajbotla23@gmail.com>
2024-10-17 19:27:48 -0400

    added

---
+++ math_quiz.py
@@ -0,0 +1,33 @@
+import random as r
+print("** Math Quiz ***")
+score=0
+i=1
+while i<=5:
+    
+    a = r.randint(1,9)
+    b = r.randint(1,9)
+    print(f"{i}.{a}+{b}=")
+    ans=int(input("enter your ans : "))
+    j=0
+    while j==0:
+        
+        
+        answer=a+b
+        if ans==a+b:
+            print("corret")
+            score +=1
+        
+            j +=1
+            
+
+            break
+        
+    
+        else:
+            print("wrong ans")
+            print(f"correct aneswer is {answer} ")
+            j +=1
+            break
+    i=i+1   
+print(f"your total score is {score}/5")    
+   
--- uncle.py
+++ uncle.py
@@ -1,26 +1,107 @@
-#
-"""while True:
-    # Input from the user
-    number = input("Enter a number (or 'stop' to quit): ")
-    
-    if number.lower() == 'stop':
-        break
-    
-    try:
-        number = int(number)
-        
-        # Check if the number is prime
-        if number < 2:
-            print(f"{number} is not a prime number.")
-        else:
-            for i in range(2, number):
-                if number % i == 0:
-                    print(f"{number} is not a prime number.")
-                    break
-            else:
-                print(f"{number} is a prime number.")
-    except ValueError:
-        print("Please enter a valid integer.")"""
+customer=[
+{"name":"John","card":12345,"pin":1234,"balance":5000},
+{"name":"Cathy","card":23456,"pin": 4567,"balance":10000}
+]     
+while True:
+    print("*** HDFC ATM ***")
+    z=input("existing customer (y/n) :")
 
-a=("hello")
-print ("hello" in a)
+
+    if z == 'n':
+        c=input("would you like to open an acoount in our bank (y/n):")
+        if c == 'y':
+            nam=input("Enter your first name :")
+            print(f" Hello {nam}")
+            import random as r
+            f=r.randint(10000,99999)
+            l=r.randint(1000,9999)            
+            print(f"your card no is {f}")
+            print(f"your pin is {l}")
+            print("your balance is  0")
+            while True:
+                    print("1. Deposit")
+                    print("2. Withdraw")
+                    print("3. Balance")
+                    print("4. Exit")
+                    balance=0
+
+
+                    choice = int(input("Enter your choice: "))
+                    
+                    if choice==1:
+                        amount=int(input("Enter Amount: "))
+                        balance=balance+amount
+                        print("You current balance is ",balance)
+                    elif choice==2:
+                        amount=int(input("Enter Amount : "))
+                        if balance<amount:
+                                print("Insufficient Balance")
+                        else:
+                            balance=balance-amount
+                            print("You current balance is ",balance)
+                    elif choice==3:
+                        print("You current balance is ",balance)
+                    elif choice==4:
+                        print("exited")
+                        break
+                    
+
+                    else:
+                        print("Invalid Input")
+
+    elif z == 'f':  # To exit the program
+        print("Thank you for using HDFC ATM!")
+        break
+
+
+    elif z =='y':
+        pass
+
+        
+            
+            
+        cardnum=int(input("Enter your card no : "))
+        passcode=int(input("Enter code : "))
+        
+
+        for i in  range(len(customer)):
+            if cardnum== customer[i]["card"] and passcode == customer[i]["pin"]:
+                print("Welcome,", customer[i]["name"])
+                balance=customer[i]["balance"]
+                print(f" your balane is {balance}")
+        
+
+
+
+                while True:
+                    print("1. Deposit")
+                    print("2. Withdraw")
+                    print("3. Balance")
+                    print("4. Exit")
+                    
+
+
+                    choice = int(input("Enter your choice: "))
+                    
+                    if choice==1:
+                        amount=int(input("Enter Amount: "))
+                        balance=balance+amount
+                        print("You current balance is ",balance)
+                    elif choice==2:
+                        amount=int(input("Enter Amount : "))
+                        if balance<amount:
+                                print("Insufficient Balance")
+                        else:
+                            balance=balance-amount
+                            print("You current balance is ",balance)
+                    elif choice==3:
+                        print("You current balance is ",balance)
+                    elif choice==4:
+                        print("exited")
+                        break
+                    
+
+                    else:
+                        print("Invalid Input")
+            else:
+                print("invald input")
