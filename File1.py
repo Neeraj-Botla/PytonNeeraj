fd5db51573af36553a1b6248e0cd2e4f05eddad3
Neeraj-Botla <neerajbotla23@gmail.com>
2024-10-18 09:22:46 -0400

    file 1 is complete

---
+++ File1.py
@@ -0,0 +1,79 @@
+def menu():
+    cardnum=int(input("Enter your card no : "))
+    passcode=int(input("Enter code : "))
+        
+
+    for i in  range(len(customer)):
+            if cardnum== customer[i]["card"] and passcode == customer[i]["pin"]:
+                print("Welcome,", customer[i]["name"])
+                balance=customer[i]["balance"]
+                print(f" your balane is {balance}")
+        
+
+
+
+                while True:
+                    print("1. Deposit")
+                    print("2. Withdraw")
+                    print("3. Balance")
+                    print("4. fund transfer")
+                    print("5.Exit")
+                    
+
+
+                    choice = int(input("Enter your choice: "))
+                    
+                    if choice==1:
+                        amount=int(input("Enter Amount: "))
+                        balance=balance+amount
+                        customer[i]["balance"]=balance
+                        print("You current balance is ",balance)
+                    elif choice==2:
+                        amount=int(input("Enter Amount : "))
+                        if balance<amount:
+                                print("Insufficient Balance")
+                        else:
+                            balance=balance-amount
+                            print("You current balance is ",balance)
+                    elif choice==3:
+                        print("You current balance is ",balance)
+                    elif choice==4:
+                        #######    
+                    elif choice==5:
+                        print("exited")
+                        break
+                    
+
+                    else:
+                        print("Invalid Input")
+
+
+customer=[
+{"name":"John","card":12345,"pin":1234,"balance":5000},
+{"name":"Cathy","card":23456,"pin": 4567,"balance":10000}
+]     
+while True:
+    print("*** HDFC ATM ***")
+    z=input("existing customer (y/n) :")
+
+
+    if z == 'n':
+        c=input("would you like to open an acoount in our bank (y/n):")
+        if c == 'y':
+            nam=input("Enter your first name :")
+            print(f" Hello {nam}")
+            import random as r
+            f=r.randint(10000,99999)
+            l=r.randint(1000,9999)            
+            print(f"your card no is {f}")
+            print(f"your pin is {l}")
+            print("your balance is  0")
+            customer.append({"name":nam,"card":f,"pin":l,"balance":0}),
+            print(customer)
+            menu()
+    else:
+        menu()
+
+            #
+                    
+            
--- hdfc_bank.py
+++ hdfc_bank.py
@@ -1,3 +1,50 @@
+def f1():
+    cardnum=int(input("Enter your card no : "))
+    passcode=int(input("Enter code : "))
+        
+
+    for i in  range(len(customer)):
+            if cardnum== customer[i]["card"] and passcode == customer[i]["pin"]:
+                print("Welcome,", customer[i]["name"])
+                balance=customer[i]["balance"]
+                print(f" your balane is {balance}")
+        
+
+
+
+                while True:
+                    print("1. Deposit")
+                    print("2. Withdraw")
+                    print("3. Balance")
+                    print("4. Exit")
+                    
+
+
+                    choice = int(input("Enter your choice: "))
+                    
+                    if choice==1:
+                        amount=int(input("Enter Amount: "))
+                        balance=balance+amount
+                        customer[i]["balance"]=balance
+                        print("You current balance is ",balance)
+                    elif choice==2:
+                        amount=int(input("Enter Amount : "))
+                        if balance<amount:
+                                print("Insufficient Balance")
+                        else:
+                            balance=balance-amount
+                            print("You current balance is ",balance)
+                    elif choice==3:
+                        print("You current balance is ",balance)
+                    elif choice==4:
+                        print("exited")
+                        break
+                    
+
+                    else:
+                        print("Invalid Input")
+
+
 customer=[
 {"name":"John","card":12345,"pin":1234,"balance":5000},
 {"name":"Cathy","card":23456,"pin": 4567,"balance":10000}
@@ -18,6 +65,8 @@ while True:
             print(f"your card no is {f}")
             print(f"your pin is {l}")
             print("your balance is  0")
+            customer.append({"name":nam,"card":f,"pin":l,"balance":0}),
+            print(customer)
             while True:
                     print("1. Deposit")
                     print("2. Withdraw")
@@ -30,8 +79,10 @@ while True:
                     
                     if choice==1:
                         amount=int(input("Enter Amount: "))
+                        #
                         balance=balance+amount
                         print("You current balance is ",balance)
+
                     elif choice==2:
                         amount=int(input("Enter Amount : "))
                         if balance<amount:
@@ -84,6 +135,7 @@ while True:
                     if choice==1:
                         amount=int(input("Enter Amount: "))
                         balance=balance+amount
+                        customer[i]["balance"]=balance
                         print("You current balance is ",balance)
                     elif choice==2:
                         amount=int(input("Enter Amount : "))
